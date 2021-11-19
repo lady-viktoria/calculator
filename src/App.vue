cd6fd214837fc0a1981d65900ad098f4838220fa
lady-viktoria <lady.vikusha@gmail.com>
2021-11-19 12:59:35 +0300

    calc lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Calculator!" :is-show="true" />
+    <Calc />
   </div>
 </template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Calc from "./components/Calc.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Calc,
+  },
+  data() {
+    return {
+      counter: 0,
+      str: "Hello!",
+    };
+  },
+  methods: {},
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +33,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,66 @@
+/* eslint-disable no-restricted-properties */
+<template>
+  <div>
+    <div>
+      <input type="number" v-model.number="op1" />
+      <input type="number" v-model.number="op2" />
+      = {{ result }}
+    </div>
+    <!-- <div class="error" v-if="error">Ошибка: {{ error }}</div> -->
+    <div>
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="div">/</button>
+      <button @click="DivInt">/(Int)</button>
+      <button @click="mult">*</button>
+      <button @click="degree">**</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calc",
+  data: () => ({
+    op1: 0,
+    op2: 0,
+    result: 0,
+    error: "",
+  }),
+  methods: {
+    sum() {
+      this.result = this.op1 + this.op2;
+    },
+    sub() {
+      this.result = this.op1 - this.op2;
+    },
+    div() {
+      const { op1, op2 } = this;
+      if (op2 === 0) {
+        this.error = "на 0 делить нельзя!";
+        this.result = `Ошибка: ${this.error}`;
+      } else {
+        // т.к. исп деструктуризация, можно исп просто op1, op2
+        this.result = op1 / op2;
+      }
+    },
+    DivInt() {
+      const { op1, op2 } = this;
+      if (op2 === 0) {
+        this.error = "на 0 делить нельзя!";
+        this.result = `Ошибка: ${this.error}`;
+      } else {
+        // т.к. исп деструктуризация, можно исп просто op1, op2
+        this.result = Math.trunc(op1 / op2);
+      }
+    },
+    mult() {
+      this.result = this.op1 * this.op2;
+    },
+    degree() {
+      // eslint-disable-next-line no-restricted-properties
+      this.result = Math.pow(this.op1, this.op2);
+    },
+  },
+};
+</script>
